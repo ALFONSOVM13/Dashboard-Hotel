172e6b2b798ce33c8e1eab99f158e3e3fb721b64
Ileana <ferchu91garcia@gmail.com>
2024-03-13 18:46:11 -0300

    botones funcionando, elementos ordenados, aun no es responsive

--- src/pages/Guests/index.jsx
+++ src/pages/Guests/index.jsx
@@ -1,6 +1,43 @@
+import { useState } from "react";
+
 function Guests() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 5; // Asumiendo que tienes 5 páginas
+
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    console.log(value); // Consologuea lo que se está escribiendo
+    setInputValue(value); // Actualiza el estado con el nuevo valor
+  };
+
+  const handleNewClick = () => {
+    console.log("hiciste click en new");
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleFirstPage = () => {
+    setCurrentPage(1);
+  };
+
+  const handleLastPage = () => {
+    setCurrentPage(totalPages);
+  };
+
   return (
-    <div className="flex flex-col px-5">
+    <div className="flex flex-col px-5 w-full">
       <div className="self-start ml-6 text-4xl font-semibold tracking-wide whitespace-nowrap text-neutral-500 max-md:ml-2.5">
         Guest Management
       </div>
@@ -14,27 +51,37 @@ function Guests() {
           type="text"
           placeholder="Name, Status, E-mail"
           className="flex-grow my-auto max-md:max-w-full bg-white"
+          value={inputValue}
+          onChange={handleInputChange}
         />
       </div>
-      <div className="flex gap-5 justify-between self-center mt-8 w-full max-w-[1026px] max-md:flex-wrap max-md:max-w-full">
-        <div className="flex flex-col flex-1 justify-center text-base font-semibold tracking-normal text-center text-white whitespace-nowrap">
-          <button className="justify-center items-center px-16 py-4 rounded shadow-sm bg-blue-950 max-md:px-5">
-            New
-          </button>
-        </div>
-        <div className="flex gap-5 justify-between px-5 max-w-[658px] max-md:flex-wrap">
-          <div className="flex gap-2.5 my-auto text-base tracking-normal whitespace-nowrap">
-            <div className="grow text-gray-500">Show results by:</div>
-            <div className="text-sky-500">10</div>
-            <button type="button" className="cursor-pointer">
-              <input
-                type="image"
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/3b834403ec30943ade687ba10871ed3dd113d3c7469decfa971c2bba0aba43b8?"
-                className="shrink-0 my-auto aspect-[2.33] fill-zinc-400 w-[20px]"
-              />
+      <div className="flex justify-around items-center self-center mt-6  w-full">
+        <div>
+          <div className="flex flex-col flex-1 justify-center text-base font-semibold tracking-normal text-center text-white whitespace-nowrap">
+            <button
+              className="justify-center items-center px-16 py-4 rounded shadow-sm bg-blue-950 max-md:px-5"
+              onClick={handleNewClick}
+            >
+              New
             </button>
           </div>
+        </div>
+        <div>
+          <div className="flex items-center gap-2.5 my-auto text-base tracking-normal whitespace-nowrap ">
+            <div className="grow text-gray-500">Show results by:</div>
+            <select
+              name="cuantity"
+              className=" bg-white text-sky-500 text-lg w-12 h-8"
+            >
+              <option value="tres">3</option>
+              <option value="cuatro">4</option>
+              <option value="cinco">5</option>
+              <option value="seis">6</option>
+              <option value="siete">7</option>
+            </select>
+          </div>
+        </div>
+        <div>
           <div className="flex gap-3.5 items-center">
             <div className="grow self-stretch my-auto text-base tracking-normal text-gray-500">
               Showing
@@ -48,47 +95,61 @@ function Guests() {
             <div className="self-stretch my-auto text-base font-bold tracking-normal text-gray-500">
               1233
             </div>
-            <div className="inline-flex -space-x-px">
+            <div className="inline-flex -space-x-* justify-center">
               <button
                 className="flex items-center justify-center cursor-pointer mx-2 px-3 h-8 text-gray-500 bg-gray-50 border border-black rounded-s-lg "
-                disabled
+                onClick={handleFirstPage}
+                disabled={currentPage === 1}
               >
                 &lt;&lt;
               </button>
               <button
                 className="flex items-center justify-center cursor-pointer mx-2 px-3 h-8 text-gray-500 bg-gray-50 border border-black rounded-s-lg "
-                disabled
+                onClick={handlePreviousPage}
+                disabled={currentPage === 1}
               >
                 &lt;
               </button>
-              <span id="page">1</span>
+              <span
+                id="page"
+                style={{
+                  display: "inline-block",
+                  marginLeft: "10px",
+                  marginRight: "10px",
+                }}
+              >
+                {currentPage}
+              </span>
               <button
                 className="flex items-center justify-center cursor-pointer mx-2 px-3 h-8 text-gray-500 bg-gray-50 border border-black rounded-s-lg "
-                disabled
+                onClick={handleNextPage}
+                disabled={currentPage === totalPages}
               >
                 &gt;
               </button>
               <button
                 className="flex items-center justify-center cursor-pointer mx-2 px-3 h-8 text-gray-500 bg-gray-50 border border-black rounded-s-lg "
-                disabled
+                onClick={handleLastPage}
+                disabled={currentPage === totalPages}
               >
                 &gt;&gt;
               </button>
             </div>
           </div>
-          <div className="flex gap-4 self-start mt-3 text-base tracking-normal">
+        </div>
+        <div>
+          <div className="flex items-center gap-4 self-start mt-3 text-base tracking-normal">
             <div className="text-gray-500">
               <span className="text-gray-500">Filter:</span>{" "}
             </div>
-            <div className="text-sky-500">All</div>
-            <button type="button" className="cursor-pointer">
-              <input
-                type="image"
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/3b834403ec30943ade687ba10871ed3dd113d3c7469decfa971c2bba0aba43b8?"
-                className="shrink-0 my-auto aspect-[2.33] fill-zinc-400 w-[20px]"
-              />
-            </button>
+
+            <select
+              name="filter"
+              className=" bg-white text-sky-500 text-lg w-full h-8"
+            >
+              <option value="active">Activ</option>
+              <option value="inactive">Inactive</option>
+            </select>
           </div>
         </div>
       </div>
