eb4962a855caa142f13812d1ea5dc6dd072e3027
Luis Alberto Camarillo Flores <luiluego@gmail.com>
2024-04-07 18:10:35 -0600

    Responsive dashboard and Protected Routes

--- src/components/Loading/Spinner/index.jsx
+++ src/components/Loading/Spinner/index.jsx
@@ -20,7 +20,7 @@ const Spinner = () => {
   return (
     <div className=" flex flex-col gap-5 justify-center items-center h-full w-full">
       <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-green-500 dark:decoration-slate-300 dark:border-slate-200"></div>
-      <span className="font-bold">{loadingText}</span>
+      <span className="font-bold dark:text-white">{loadingText}</span>
     </div>
   );
 };
