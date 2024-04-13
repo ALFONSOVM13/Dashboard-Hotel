a4193dd2d506dded8e3abf18c60fde8688a2ce62
Ileana <ferchu91garcia@gmail.com>
2024-04-13 00:21:27 -0300

    vista servicios y reducer

---
+++ src/dataService.js
@@ -0,0 +1,36 @@
+const dataServices = [
+  {
+    type: "reserva de auto",
+    imageUrl: "imagen_auto1.jpg",
+    name: "Reserva de Auto Económico",
+    price: "$50 por día",
+    description:
+      "Reserva un auto económico para tu viaje. Perfecto para desplazamientos urbanos y excursiones cortas.",
+  },
+  {
+    type: "reserva de auto",
+    imageUrl: "imagen_auto2.jpg",
+    name: "Reserva de Auto de Lujo",
+    price: "$150 por día",
+    description:
+      "Reserva un auto de lujo para viajar con estilo. Equipado con las últimas comodidades y tecnología.",
+  },
+  {
+    type: "masaje",
+    imageUrl: "imagen_masaje1.jpg",
+    name: "Masaje Relajante",
+    price: "$60 por sesión",
+    description:
+      "Disfruta de un masaje relajante que te ayudará a liberar tensiones y revitalizar tu cuerpo y mente.",
+  },
+  {
+    type: "masaje",
+    imageUrl: "imagen_masaje2.jpg",
+    name: "Masaje Terapéutico",
+    price: "$80 por sesión",
+    description:
+      "Un masaje terapéutico diseñado para aliviar dolores musculares y mejorar la circulación sanguínea.",
+  },
+];
+
+export default dataServices;
